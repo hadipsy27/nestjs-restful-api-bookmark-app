e2470395717c01558705de3571aac5b44e6b6313
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-05-05 23:16:41 +0700

    signup custom error message if email duplicate in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 
 @Injectable()
@@ -12,16 +13,24 @@ export class AuthService{
         // generate the password hash
         const hash = await argon.hash(dto.password);
         //save new user in db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-        });
-
-        delete user.hash;
-        // return the saved user
-        return user;
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+    
+            delete user.hash;
+            // return the saved user
+            return user;
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code === 'P2002'){ // P2002 -> is duplicate field
+                    throw new ForbiddenException('Credentials taken')
+                }
+            }
+        }
     }
 
     signin(){
