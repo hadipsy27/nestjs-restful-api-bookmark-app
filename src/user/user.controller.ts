ade6af467c1b2f2161ac8e0b0192df5e61c66676
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-05-06 21:33:40 +0700

    login with jwt and pasport js - auth guard

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('users')
+export class UserController {
+  constructor(){}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get("me")
+  getMe(){
+    return "Hello, this is User info..";
+  }
+}
