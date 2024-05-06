5d9dfcaea666b8404222e4add74739cfd6509f4c
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-05-06 21:55:59 +0700

    new class jwtGuard to save param AuthGuard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/guard';
 
 @Controller('users')
 export class UserController {
   constructor(){}
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get("me")
   getMe(@Req() req: Request){
     console.log({
